b36c6a9b487147dfdf992e1173b33f1bfabb3bc3
kunalsahni99 <kunal.sahni1999@gmail.com>
2025-08-15 14:08:23 +0530

    Configured app URLs

--- poll_app/urls.py
+++ poll_app/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from polls.views import home
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("home/", home)
+    path("polls/", include("polls.urls"))
 ]
